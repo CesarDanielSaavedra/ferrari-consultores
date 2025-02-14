ba739ad8193d3f946bbff81a935c2c16e0526cd8
Daniel Saavedra <cesardsaavedra@gmai.com>
2025-02-14 10:02:06 -0300

    usefull icon added. Vite config has been changed to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    base: mode === 'production' ? '/ser-unidad/' : '/',
+    base: mode === 'production' ? '/ferrari-consultores/' : '/',
     plugins: [react()],
   };
 });
